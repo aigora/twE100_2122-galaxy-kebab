d390a7c8a86ef645180eb9817250abff3e473b26
jlopezlavina <jorge.lopez.lavina@alumnos.upm.es>
2022-05-06 12:49:31 +0200

    Create dificil.c

---
+++ GALAXYKEBAB/dificil.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "dificil.h"
+#include "facil.h"
+void juegodificil()
+{
+    char decision;
+    int vidas[0]={3};
+    sala3(vidas);
+
+}
+
+void sala3(int vidas[])
+{
+    char letra;
+    char palabra[20];
+
+    system("PAUSE");
+    printf("Se cierra la pesada puerta metalica detras de ti con un sonido seco.\n");
+    printf("Tratas de volver a abrirla pero esta bloqueada y no puedes.\n");
+    system("PAUSE");
+    printf("No ves nada. Te encuentras en una habitacion oscura, sin luces ni ventanas\n");
+    printf("Empiezas a tocar todas las paredes tratando de encontrar un interruptor.\n");
+    system("PAUSE");
+    printf("Y de repente,... EUREKA!\n");
+    printf("Has encontrado lo que parece un cuadro de luces, pero...\n");
+    system("PAUSE");
+    printf("Para poder encender los plomos deberas contestar una pregunta.\n");
+    printf("");
+    printf("Se encienden las luces de repente y quedas cegado unos segundos.\n");
+
+}
+void sala4(int vidas[])
+{
+
+}
