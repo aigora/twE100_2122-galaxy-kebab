19395ae43067794ab5fc227f42abfe2247e3afc8
jlopezlavina <jorge.lopez.lavina@alumnos.upm.es>
2022-05-11 09:34:22 +0200

    Update dificil.c
    pregunta y respuesta 1

--- galaxy kebab gorje/dificil.c
+++ galaxy kebab gorje/dificil.c
@@ -6,7 +6,7 @@
 void juegodificil()
 {
     char decision;
-    int vidas[0]={3};
+    int vidas[1]={3};
     sala3(vidas);
 
 }
@@ -15,11 +15,12 @@ void sala3(int vidas[])
 {
     char letra;
     char palabra[20];
+    char pregunta1[]="Edison";
+    int a ;
 
     system("PAUSE");
     printf("Se cierra la pesada puerta metalica detras de ti con un sonido seco.\n");
     printf("Tratas de volver a abrirla pero esta bloqueada y no puedes.\n");
-    system("PAUSE");
     printf("No ves nada. Te encuentras en una habitacion oscura, sin luces ni ventanas\n");
     printf("Empiezas a tocar todas las paredes tratando de encontrar un interruptor.\n");
     system("PAUSE");
@@ -27,12 +28,32 @@ void sala3(int vidas[])
     printf("Has encontrado lo que parece un cuadro de luces, pero...\n");
     system("PAUSE");
     printf("Para poder encender los plomos deberas contestar una pregunta.\n");
+    system("PAUSE");
+    printf("Quien fue el inverntor de la bombilla incandescente? y recuerda que los nombres propios empiezan en mayuscula.\n");
 
-    printf("");
+    do
+    {
+    fflush(stdin);
+    scanf("%s",palabra);
+    a=strcmp(pregunta1,palabra);
+     if(a==1)
+     {
+     printf("MAL.\n");
+     printf("Tienes %d vidas",puntosdevidarestafac(vidas));
+     }
+    }while(a==1);
+    printf("CORRECTO.\n");
+    printf("Tienes %d vidas.\n",vidas[0]);
+
+
     printf("Se encienden las luces de repente y quedas cegado unos segundos.\n");
+    printf(".\n");
 
+
+
+    sala3(vidas);
 }
 void sala4(int vidas[])
 {
-
+printf("dkjfhgdjskldñfkgjnbjdnkzml,ñzcvlmkbn");
 }
