a5f4ec3595b039c7f80a3190389b14e3a3e529e6
AlexGG03 <alex.guerrero@alumnos.upm.es>
2022-05-12 10:39:58 +0200

    Update dificil.c
    he actualizado la sala,para que haya intentos limitados,y sus bonificaciones y penalizaciones correspondientes

--- galaxy kebab gorje/dificil.c
+++ galaxy kebab gorje/dificil.c
@@ -13,14 +13,13 @@ void juegodificil()
 
 void sala3(int vidas[])
 {
-    char letra;
     char palabra[20];
+    int a,j=0,h=0;
     char pregunta1[]="Edison";
-    int a ;
-
     system("PAUSE");
     printf("Se cierra la pesada puerta metalica detras de ti con un sonido seco.\n");
     printf("Tratas de volver a abrirla pero esta bloqueada y no puedes.\n");
+    system("PAUSE");
     printf("No ves nada. Te encuentras en una habitacion oscura, sin luces ni ventanas\n");
     printf("Empiezas a tocar todas las paredes tratando de encontrar un interruptor.\n");
     system("PAUSE");
@@ -28,30 +27,45 @@ void sala3(int vidas[])
     printf("Has encontrado lo que parece un cuadro de luces, pero...\n");
     system("PAUSE");
     printf("Para poder encender los plomos deberas contestar una pregunta.\n");
-    system("PAUSE");
+     system("PAUSE");
     printf("Quien fue el inverntor de la bombilla incandescente? y recuerda que los nombres propios empiezan en mayuscula.\n");
-
-    do
+   do
     {
-    fflush(stdin);
     scanf("%s",palabra);
-    a=strcmp(pregunta1,palabra);
-     if(a==1)
-     {
-     printf("MAL.\n");
-     printf("Tienes %d vidas",puntosdevidarestafac(vidas));
-     }
-    }while(a==1);
-    printf("CORRECTO.\n");
+    a=strcmp(palabra,pregunta1);
+    if(a!=0)
+    {
+    h++;
+    printf("MAL ,Te quedan %d intentos.\n",3-h);
+    if(h==3)
+    {
+        vidas[0]=puntosdevidarestafac(vidas);
+    }
+    j++;
+    }
+    if(a==0)
+    {
+        printf("CORRECTO.\n");
+         if(h==0)
+        {
+            vidas[0]=puntosdevidasumafac(vidas);
+        }
+    }
+    }while((a!=0)&&(j<3));
     printf("Tienes %d vidas.\n",vidas[0]);
+    if(vidas[0]==0 || vidas[0]<0)
+    {
+        fin();
+    }
+    else
+        {
 
 
     printf("Se encienden las luces de repente y quedas cegado unos segundos.\n");
-    printf(".\n");
 
+    sala4(vidas);
+    }
 
-
-    sala3(vidas);
 }
 void sala4(int vidas[])
 {
