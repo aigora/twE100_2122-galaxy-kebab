a29ba3559c6b34a1d8c0bdfb9419456e18e76dfa
AlexGG03 <alex.guerrero@alumnos.upm.es>
2022-05-24 13:36:13 +0200

    MENU

--- menu.c
+++ menu.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include "instrucciones.h"
+#include "login.h"
+#include "facil.h"
+#include "dificil.h"
 int main()
 {
-int op1,op2,op3,op4,op5;
+int op;
+char printnick[100];
+int numsuarios=0;
+usuario datos[100];
  printf("BIENVENIDO A GALAXY KEBAB.\n");
  printf("\n");
- printf("Usted tiene varias opciones en este menu como por ejemplo:\n");
+ printf("Estas en el menu,elija una opcion por favor:\n");
  printf("\n");
  printf("Pulse 1 si quiere iniciar el juego.\n");
  printf("Pulse 2 si quiere salir del juego.\n");
  do
  {
- scanf("%d",&op1);
+     fflush(stdin);
+ scanf("%d",&op);
  }
- while((op1<1)||(op1>2));
- switch(op1)
+ while(op!=1&&op!=2);
+ switch(op)
  {
  case 1:
      {
@@ -22,42 +31,59 @@ int op1,op2,op3,op4,op5;
          printf("Debe registrarse (pulsando 1) o iniciar sesion (pulsando 2).\n");
          do
          {
-         scanf("%d",&op2);
+             fflush(stdin);
+         scanf("%d",&op);
          }
-         while((op2<1)||(op2>2));
-         switch(op2)
+         while((op!=1)&&(op!=2));
+         switch(op)
          {
          case 1:
              {
-                 printf("Introduzca el nombre (maximo 10 caracteres): \n");
-                 printf("Introduzca la contraseña: \n");
-                 break;
+                if(reconocerfich(datos,&numsuarios)==0)
+                    {
+                        return 0; //se acaba el programa si no hay fichero
+
+				    }
+				else
+				{
+					registrar(printnick,&numsuarios,datos);
+				}
+				break;
              }
          case 2:
             {
-                printf("Introduzca el nombre: \n");
-                 printf("Introduzca la contraseña: \n");
-                break;
+               if(reconocerfich(datos,&numsuarios)==0)
+                {
+                 return 0; //se acaba el programa si no hay fichero
+				}
+				else
+				{
+				login(printnick,&numsuarios,datos);
+				}
+				break;
             }
          }
      }
 
  printf("\n");
- inicio1:
  printf("Despues de haber iniciado sesion o haberse registrado en galaxy kebab. Toca pasar al siguiente paso.\n");
+ inicio1:
  printf("Pulse 1 si desea leer las intrucciones del juego.\n");
  printf("Pulse 2 si desea empezar a jugar.\n");
  do
  {
- scanf("%d",&op3);
+     fflush(stdin);
+ scanf("%d",&op);
  }
- while((op3<1)||(op3>2));
- switch(op3)
+ while((op!=1)&&(op!=2));
+ switch(op)
  {
  case 1:
      {
     printf("Aqui estan las instrucciones: \n");
     printf("\n");
+    printins(); //funcion de instrucciones
+    system ("cls");
     goto inicio1;
     break;
      }
@@ -74,25 +100,28 @@ int op1,op2,op3,op4,op5;
  printf("Pulse 2 si quiere escoger el nivel de dificultad dificil.\n");
  do
  {
- scanf("%d",&op4);
+fflush(stdin);
+scanf("%d",&op);
  }
- while((op4<1)||(op4>2));
- switch(op4)
+ while((op!=1)&&(op!=2));
+ switch(op)
  {
    case 1:
        {
-           printf("Ha escogido el nivel facil.\n");
-           printf("Por tanto, durante el juego se encontrara con preguntas asequibles.\n");
-            printf("......................................................................\n");
-        printf("Comienza la gran experiencia de el scape room GALAXY KEBAB");
+           system("cls");
+        printf("Ha escogido el nivel facil.\n");
+        printf("Por tanto, durante el juego se encontrara con preguntas asequibles.\n");
+        printf("Comienza la gran experiencia de el scape room GALAXY KEBAB.\n");
+        juegofacil();
            break;
        }
    case 2:
     {
+        system("cls");
         printf("Ha escogido el nivel dificil.\n");
         printf("Por tanto,durante el juego se encontrara con preguntas mas complejas.\n");
-        printf("......................................................................\n");
-        printf("Comienza la gran experiencia de el scape room GALAXY KEBAB\n");
+        printf("Comienza la gran experiencia de el scape room GALAXY KEBAB.\n");
+        juegodificil();
         break;
     }
  }
@@ -104,10 +133,11 @@ int op1,op2,op3,op4,op5;
  printf("Pulse 2 si desea acabar.\n");
 do
  {
- scanf("%d",&op5);
+fflush(stdin);
+scanf("%d",&op);
  }
- while((op5<1)||(op5>2));
- switch(op5)
+ while((op!=1)&&(op!=2));
+ switch(op)
  {
  case 1:
      {
@@ -121,8 +151,11 @@ do
  }
  case 2:
     {
+        //aqui va ir los creditos.....
         printf("Hasta pronto!\n");
         break;
     }
 }
+return 0;
 }
+
